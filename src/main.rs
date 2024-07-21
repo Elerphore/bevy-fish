d6fd3b1aec9800af71308fcb6ff1c7f62a3be5a0
Elerphore <Elerphore@gmail.com>
2024-07-21 10:12:29 +0300

    screen bounce

--- src/main.rs
+++ src/main.rs
@@ -2,24 +2,35 @@ use bevy::{
     app::{App, FixedUpdate, Startup},
     asset::Assets,
     color::Color,
+    log::info,
     math::{Vec3, Vec3Swizzles},
     prelude::{Camera2dBundle, Circle, Commands, Component, IntoSystemConfigs, Query, ResMut},
-    render::mesh::Mesh,
+    render::{mesh::Mesh, view::window},
     sprite::{ColorMaterial, MaterialMesh2dBundle, Mesh2dHandle},
     transform::components::Transform,
+    utils::tracing::field::debug,
+    window::Window,
     DefaultPlugins,
 };
 use rand::{rngs::ThreadRng, thread_rng, Rng};
 
 const _X_EXTENT: f32 = 900.;
+
 const MAX_SPEED: f32 = 6.0;
 const MIN_SPEED: f32 = 3.0;
+const TURN_FACTOR: f32 = 1.0;
+const AVOID_FACTOR: f32 = 1.5;
+const CENTERING_FACTOR: f32 = 0.5;
+const COHESION_FACTOR: f32 = 0.5;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_systems(Startup, setup)
-        .add_systems(FixedUpdate, (avoid_factor, speed_limiter, move_fish).chain())
+        .add_systems(
+            FixedUpdate,
+            (avoid_factor, speed_limiter, screen_bounce, move_fish).chain(),
+        )
         .run();
 }
 
@@ -29,7 +40,7 @@ fn setup(
     mut materials: ResMut<Assets<ColorMaterial>>,
 ) {
     let mut rnd: ThreadRng = thread_rng();
-    let circle = Circle { radius: 32.0 };
+    let circle = Circle { radius: 8.0 };
     let color = Color::hsl(360., 0.95, 0.7);
 
     for _ in 0..10 {
@@ -55,14 +66,18 @@ fn setup(
 }
 
 fn move_fish(mut query: Query<(&mut Transform, &mut Fish)>) {
-    query.iter_mut().for_each(|(mut transform, mut fish)| {
+    query.iter_mut().for_each(|(mut transform, fish)| {
         transform.translation.x += fish.vec.x;
         transform.translation.y += fish.vec.y;
     })
 }
 
+fn aligment_factor() {}
+
+fn cohesion_factor() {}
+
 fn speed_limiter(mut query: Query<(&mut Transform, &mut Fish)>) {
-    query.iter_mut().for_each(|(mut transform, mut fish)| {
+    query.iter_mut().for_each(|(_transform, mut fish)| {
         let speed = (fish.vec.x * fish.vec.x + fish.vec.y * fish.vec.y).sqrt();
 
         if speed > MAX_SPEED {
@@ -79,15 +94,54 @@ fn speed_limiter(mut query: Query<(&mut Transform, &mut Fish)>) {
 
 fn avoid_factor(mut query: Query<&mut Fish>) {
     let mut iter = query.iter_combinations_mut();
-    let avoidfactor = 0.05;
 
     while let Some([mut fish_one, fish_two]) = iter.fetch_next() {
         let close_dx = fish_one.vec.x - fish_two.vec.x;
         let close_dy = fish_one.vec.y - fish_two.vec.y;
 
-        fish_one.vec.x += close_dx * avoidfactor;
-        fish_one.vec.y += close_dy * avoidfactor;
+        fish_one.vec.x += close_dx * AVOID_FACTOR;
+        fish_one.vec.y += close_dy * AVOID_FACTOR;
     }
+}
+
+fn screen_bounce(windows: Query<&Window>, mut query: Query<(&mut Fish, &Transform)>) {
+    let window = windows.single();
+    let height = window.height();
+    let width = window.width();
+
+    info!("height: {}, width: {}", height, width);
+
+    let left = width / -2.0;
+    let right = width / 2.0;
+    let bottom = height / -2.0;
+    let top = height / 2.0;
+
+    const MARGIN: f32 = 200.0;
+
+    info!(
+        "left: {}, right: {}, top: {}, bottom: {}",
+        left, right, bottom, top
+    );
+
+    query.iter_mut().for_each(|(mut f, t)| {
+        info!("fish x: {}, y: {}", t.translation.x, t.translation.y);
+
+        if t.translation.x < left + MARGIN {
+            f.vec.x += TURN_FACTOR;
+        }
+
+        if t.translation.x > right - MARGIN {
+            f.vec.x -= TURN_FACTOR;
+        }
+
+        if t.translation.y < top + MARGIN {
+            f.vec.y += TURN_FACTOR;
+        }
+
+        if t.translation.y > bottom - MARGIN {
+            f.vec.y -= TURN_FACTOR;
+        }
+    })
 }
 
 #[derive(Component, Debug, Clone, Copy)]
