38048742a9fce1a5a157f43c6a1e6b40aee6b862
Elerphore <Elerphore@gmail.com>
2024-07-23 20:37:22 +0300

    egui, sliders

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,33 @@
 use bevy::{
-    app::{App, FixedUpdate, Startup},
+    app::{App, FixedUpdate, Startup, Update},
     asset::Assets,
     color::Color,
     math::{Vec2, Vec3, Vec3Swizzles},
-    prelude::{Camera2dBundle, Circle, Commands, Component, IntoSystemConfigs, Query, ResMut},
+    prelude::{
+        Camera2dBundle, Circle, Commands, Component, IntoSystemConfigs, Query, ResMut, Resource,
+    },
     render::mesh::Mesh,
     sprite::{ColorMaterial, MaterialMesh2dBundle, Mesh2dHandle},
     transform::components::Transform,
     window::Window,
     DefaultPlugins,
 };
+use bevy_egui::{egui, EguiContexts, EguiPlugin};
 use rand::{rngs::ThreadRng, thread_rng, Rng};
 
 const MAX_SPEED: f32 = 5.0;
 const MIN_SPEED: f32 = 2.0;
-const AVOID_FACTOR: f32 = 0.1;
-const MATCHING_FACTOR: f32 = 0.05;
-const CENTERING_FACTOR: f32 = 0.01;
 const NEIGHBOR_RADIUS: f32 = 50.0;
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins)
+        .add_plugins((DefaultPlugins, EguiPlugin))
+        .init_resource::<WorldState>()
         .add_systems(Startup, setup)
+        .add_systems(Update, ui_example_system)
         .add_systems(
             FixedUpdate,
-            (
+            ((
                 avoid_factor,
                 aligment_factor_avg_vector,
                 aligment_factor,
@@ -34,12 +36,32 @@ fn main() {
                 speed_limiter,
                 move_fish,
             )
-                .chain(),
+                .chain(),),
         )
         .run();
 }
 
+fn ui_example_system(mut contexts: EguiContexts, mut world_state: ResMut<WorldState>) {
+    egui::Window::new("Hello").show(contexts.ctx_mut(), |ui| {
+        ui.label("avoid_factor");
+        ui.add(egui::Slider::new(&mut world_state.avoid_factor, 0.01..=1.0));
+
+        ui.label("matching_factor");
+        ui.add(egui::Slider::new(
+            &mut world_state.matching_factor,
+            0.01..=0.1,
+        ));
+
+        ui.label("centering_factor");
+        ui.add(egui::Slider::new(
+            &mut world_state.centering_factor,
+            0.0005..=0.001,
+        ));
+    });
+}
+
 fn setup(
+    mut world_state: ResMut<WorldState>,
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<ColorMaterial>>,
@@ -48,7 +70,11 @@ fn setup(
     let circle = Circle { radius: 8.0 };
     let color = Color::hsl(360., 0.95, 0.7);
 
-    for _ in 0..299 {
+    world_state.matching_factor = 0.5;
+    world_state.avoid_factor = 0.05;
+    world_state.centering_factor = 0.0005;
+
+    for _ in 0..99 {
         let transform = Transform::from_xyz(
             rnd.gen_range(-100.0..100.0),
             rnd.gen_range(-100.0..100.0),
@@ -82,9 +108,9 @@ fn move_fish(mut query: Query<(&mut Transform, &mut Fish)>) {
     }
 }
 
-fn aligment_factor(mut query: Query<(&mut Fish, &MetaData)>) {
+fn aligment_factor(mut query: Query<(&mut Fish, &MetaData)>, world_state: ResMut<WorldState>) {
     for (mut fish, metadata) in query.iter_mut() {
-        let vec = (metadata.x_vel_avg - fish.vec.extend(0.0).xy()) * MATCHING_FACTOR;
+        let vec = (metadata.x_vel_avg - fish.vec.extend(0.0).xy()) * world_state.matching_factor;
         fish.vec += vec;
     }
 }
@@ -106,9 +132,12 @@ fn aligment_factor_avg_vector(mut query: Query<(&mut Transform, &mut Fish, &mut 
     }
 }
 
-fn cohesion_factor(mut query: Query<(&mut Transform, &mut MetaData, &mut Fish)>) {
+fn cohesion_factor(
+    mut query: Query<(&mut Transform, &mut MetaData, &mut Fish)>,
+    world_state: ResMut<WorldState>,
+) {
     for (transform, metadata, mut fish) in query.iter_mut() {
-        let vec = (metadata.x_pos_avg - transform.translation.xy()) * CENTERING_FACTOR;
+        let vec = (metadata.x_pos_avg - transform.translation.xy()) * world_state.centering_factor;
         fish.vec += vec;
     }
 }
@@ -127,14 +156,15 @@ fn speed_limiter(mut query: Query<(&mut Transform, &mut Fish)>) {
     }
 }
 
-fn avoid_factor(mut query: Query<&mut Fish>) {
+fn avoid_factor(mut query: Query<&mut Fish>, world_state: ResMut<WorldState>) {
     let mut iter = query.iter_combinations_mut();
 
     while let Some([mut fish_one, fish_two]) = iter.fetch_next() {
         let distance = (fish_one.vec - fish_two.vec).length();
 
         if distance < NEIGHBOR_RADIUS {
-            let avoidance_vector = (fish_one.vec - fish_two.vec).normalize() * AVOID_FACTOR;
+            let avoidance_vector =
+                (fish_one.vec - fish_two.vec).normalize() * world_state.avoid_factor;
             fish_one.vec += avoidance_vector;
         }
     }
@@ -184,3 +214,10 @@ struct MetaData {
 
 #[derive(Component)]
 struct CameraMarker;
+
+#[derive(Default, Resource)]
+struct WorldState {
+    avoid_factor: f32,
+    centering_factor: f32,
+    matching_factor: f32,
+}
